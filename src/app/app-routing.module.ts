2eac6e5b0bac7774fbfbeaae8dbdd3a7c192edf2
William Silva <williamm.hpx@gmail.com>
2021-06-19 11:32:29 -0300

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { LeisPageComponent } from './componentes/leis-page/leis-page.component';
+import { InstituicoesPageComponent } from './componentes/instituicoes-page/instituicoes-page.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MenuComponent,
+    children:[
+      {
+        path:"",
+        component: HomeComponent
+      },
+      {
+        path:"intituicoes",
+        component: InstituicoesPageComponent
+      },
+      {
+        path:"leis",
+        component: LeisPageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
