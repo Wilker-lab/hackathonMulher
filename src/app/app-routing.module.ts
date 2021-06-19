9be63697864aec370ea22f8b995d4efa84f387c1
Stephanie de Castro Gonçalves <dev.stephanie.castro@gmail.com>
2021-06-19 11:41:02 -0300

    menu update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-        path:"intituicoes",
+        path:"instituicoes",
         component: InstituicoesPageComponent
       },
       {
